a35e705c5a58c22937838b57ea0a5d02f38a92a4
Elie Vangu <vangu.elie@gmail.com>
2020-08-08 20:33:28 +0200

    first commit

---
+++ caller/contracts/CallerContract.sol
@@ -0,0 +1,12 @@
+pragma solidity 0.5.0;
+
+//1. Import from the "./EthPriceOracleInterface.sol" file
+import "./EthPriceOracleInterface.sol";
+contract CallerContract {
+  // 2. Declare `EthPriceOracleInterface`
+  address private oracleAddress;
+  function setOracleInstanceAddress(address _oracleInstanceAddress) public {
+    oracleAddress = _oracleInstanceAddress;
+    //3. Instantiate `EthPriceOracleInterface`
+  }
+}
