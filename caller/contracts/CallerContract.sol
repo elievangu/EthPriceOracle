173f9397d59dd2a8bd81814c81350825b0236930
Elie Vangu <vangu.elie@gmail.com>
2020-08-09 01:12:14 +0200

    import of Ownable + onlyOwner + event creation

--- caller/contracts/CallerContract.sol
+++ caller/contracts/CallerContract.sol
@@ -1,15 +1,26 @@
 pragma solidity 0.5.0;
 
-//1. Import from the "./EthPriceOracleInterface.sol" file
+// Import from the "./EthPriceOracleInterface.sol" file
 import "./EthPriceOracleInterface.sol";
 
-contract CallerContract {
-  // 2. Declare `EthPriceOracleInterface`
+// Import the contents of "openzeppelin-solidity/contracts/ownership/Ownable.sol"
+import "openzeppelin-solidity/contracts/ownership/Ownable.sol;"
+contract CallerContract is Ownable {
+  
+  // Declare `EthPriceOracleInterface`
   EthPriceOracleInterface private oracleInstance;
   address private oracleAddress;
-  function setOracleInstanceAddress(address _oracleInstanceAddress) public {
+  // Declaration of an event named newOracleAddressEvent
+  event newOracleAddressEvent(address oracleAddress);
+  
+  // Add the `onlyOwner` modifier to the `setOracleInstanceAddress` function definition
+  function setOracleInstanceAddress(address _oracleInstanceAddress) public onlyOwner {
     oracleAddress = _oracleInstanceAddress;
-    //3. Instantiate `EthPriceOracleInterface`
+    
+    // Instantiate `EthPriceOracleInterface`
     oracleInstance = EthPriceOracleInterface(oracleAddress);
+    
+    // Fire `newOracleAddressEvent`
+    newOracleAddressEvent(oracleAddress);
   }
 }
