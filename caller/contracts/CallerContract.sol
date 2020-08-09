10cac0068fe348af580e56ef8ff41e908fd87264
Elie Vangu <vangu.elie@gmail.com>
2020-08-09 19:40:56 +0200

    add function updateEthPrice + mapping myRequests

--- caller/contracts/CallerContract.sol
+++ caller/contracts/CallerContract.sol
@@ -5,16 +5,24 @@ import "./EthPriceOracleInterface.sol";
 
 // Import the contents of "openzeppelin-solidity/contracts/ownership/Ownable.sol"
 import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
+
 contract CallerContract is Ownable {
   
   // Declare `EthPriceOracleInterface`
   EthPriceOracleInterface private oracleInstance;
   address private oracleAddress;
+
+  // Mapping that set every request id status as true or false
+  mapping(uint256 => bool) myRequests;
+
   // Declaration of an event named newOracleAddressEvent
   event newOracleAddressEvent(address oracleAddress);
+
+  // Declaration of an event named ReceivedNewRequestIdEvent
+  event ReceivedNewRequestIdEvent(uint256 id);
   
   // Add the `onlyOwner` modifier to the `setOracleInstanceAddress` function definition
-  function setOracleInstanceAddress(address _oracleInstanceAddress) public onlyOwner {
+  function setOracleInstanceAddress (address _oracleInstanceAddress) public onlyOwner {
     oracleAddress = _oracleInstanceAddress;
     
     // Instantiate `EthPriceOracleInterface`
@@ -22,5 +30,13 @@ contract CallerContract is Ownable {
     
     // Fire `newOracleAddressEvent`
     emit newOracleAddressEvent(oracleAddress);
+  }
+
+  // Define the `updateEthPrice` function
+  function updateEthPrice () public {
+    
+    uint256 id = oracleInstance.getLatestEthPrice();
+    myRequests[id] = true;
+    emit ReceivedNewRequestIdEvent(id);
   }
 }
